918810d6721452ab6aa9294c4333f5f46316b726
gihrig <glen@arkadias.com>
2024-10-05 17:35:51 -0700

    fix(install.sh): add sudo to homebrew install

--- install.sh
+++ install.sh
@@ -198,7 +198,7 @@ setup_homebrew() {
   if test ! "$(command -v brew)"; then
     info "Homebrew not installed. Installing."
     # Run as a login shell (non-interactive) so that the script doesn't pause for user input
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+    sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
   fi
 
   if [ "$(uname)" == "Linux" ]; then
