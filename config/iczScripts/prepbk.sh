ed4c8949dcb5cfcf6eb14cc38b06c8cfaa00dbc5
gihrig <glen@arkadias.com>
2025-01-01 21:09:46 -0800

    chore(prepbk.sh): update backup for name to 2025

--- config/iczScripts/prepbk.sh
+++ config/iczScripts/prepbk.sh
@@ -8,7 +8,7 @@ diskutil mount iCrumzData
 #/opt/homebrew/opt/mysql/bin/mysqld_safe --datadir\=/Volumes/iCrumzData/iCrumz/MySQLData
 
 # Change to backup directory
-cd /Volumes/iCrumzData/iCrumz/Backup/2024
+cd /Volumes/iCrumzData/iCrumz/Backup/2025
 
 # List most recent backup file
 ls -l | tail -n1
